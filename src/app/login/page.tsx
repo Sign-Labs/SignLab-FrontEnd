0c1af4774cea218d3790b194f21b489a731cc579
Chayathon.R <162368012+ThirdChyr@users.noreply.github.com>
2025-06-25 01:34:55 +0700

    adding component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,34 @@
 "use client";
 import { AiOutlineLeft } from "react-icons/ai";
-import styles from "../css/page.module.css"; 
 import "../css/component.css"; 
-import "../css/central.css"; // Corrected import statement
+import "../css/container.css";
+
 
 export default function Login() {
    
     return (
-        <main className="first_container_outer">
-            <div className="first_container_top">
+        <main className="container_outer">
+            <div className="container_top">
                 <button onClick={() => window.history.back()}style={{background: "transparent", border: "none",padding: 0,cursor: "pointer"}}>
                  <AiOutlineLeft size={45} style={{ strokeWidth: 30, color: "#171717"}} />
                 </button>
             </div>
             <div className="login_container_outer">
-                    <h1 className={styles.title}>เข้าสู่ระบบ</h1>
+                <div className="container_bottom" style={{marginTop : -20}}>
+                    <h1 className="font_heading">เข้าสู่ระบบ</h1>
+                </div>
                 <div className="login_container_inner">
                     <img src={"./picintro.png"} alt="Introduction" />
                     <div className="login_container_inner_right">
-                        <input type="email" placeholder="อีเมล" className="primary_input_box" />
+                        <input type="email" placeholder="อีเมล" className="input_button" />
+                        <input type="password" placeholder="รหัสผ่าน" className="input_button" />
+                        <div style={{justifyContent: "center",alignItems: "center",display: "flex",flexDirection: "column"}}>
+                        <button className="first_button_getstart">
+                            <h1 className="font_description_white normal">เข้าสู่ระบบ</h1>
+                            </button>
+                        
+                        <h1 className="font_description light">ลืมรหัสผ่าน?</h1>
+                        </div>
                     </div>
                 </div>
                 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import "./css/container.css"; 
 import "./css/component.css";
-import styles from "./css/page.module.css";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -12,19 +11,19 @@ export default function Home() {
   }
   
   return (
-    <main className="first_container_outer">
-      <div className="first_container_top">
-        <h1 className="font_heading">Sign Lab</h1>
+    <main className="container_outer">
+      <div className="container_top">
+        <h1 className="font_heading bold">Sign Lab</h1>
         <button className="first_button_register" onClick={() => handleClick("login")}>
-          <h1 className="first_Text_Account">ฉันมีบัญชีอยู่แล้ว</h1>
+          <h1 className="font_description normal">ฉันมีบัญชีอยู่แล้ว</h1>
         </button>
       </div>
-      <div className="first_container_bottom">
+      <div className="container_bottom">
         <img src={"./picintro.png"} alt="Introduction" />
         <div className="first_container_bottom_left">
           <h1 className="font_heading_Skyblue">เรียนภาษามือง่าย ๆ ได้ทุกที่ ทุกเวลา</h1>
-          <button className={styles.next_button} onClick={() => handleClick("register")}>
-            <h1 className={styles.start_text}>มาเริ่มกันเลย</h1>
+          <button className="first_button_getstart"onClick={() => handleClick("register")}>
+            <h1 className="font_description_white normal">มาเริ่มกันเลย</h1>
           </button>
         </div>
       </div>
