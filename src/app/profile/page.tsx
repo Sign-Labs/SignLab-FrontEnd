7f748926f2d4116339395ea5154f5a03e738d9f7
Chayathon.R <162368012+ThirdChyr@users.noreply.github.com>
2025-07-10 04:31:32 +0700

    Create page waiting for natty

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+export default function profile()
+{
+    return (
+        <h1>this is profile page</h1>
+    );
+}
---
+++ src/app/ranking/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+export default function ranking()
+{
+    return (
+        <h1>this is ranking page</h1>
+    );
+}
